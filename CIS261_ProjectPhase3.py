74d7daf00f8d9a8ad11e5fba0b13953693772b66
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-02-14 15:56:31 -0800

    CIS261 ProjectPhase3

--- CIS261_ProjectPhase3.py
+++ CIS261_ProjectPhase3.py
@@ -58,7 +58,7 @@ def printinfo(DetailsPrinted):
     TotNetPay = 0.00
 #****************************************************************************************************************************
     # write the line of code that will open the file in read mode and assign it to EmpFile (Hint: see week 6, lab 2 as a guide)
-    with open(FILENAME, "r" ) as EmpFiles:
+    with open(FILENAME, "r" ) as EmpFile:
         while True:
             rundate = input ("Enter start date for report (MM/DD/YYYY) or All for all data in file: ")
             if (rundate.upper() == "ALL"):
